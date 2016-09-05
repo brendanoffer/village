ed28f672f8f6b19c96fc309d4299967361f14642
https://github.com/settings/profile <brendanoffer@gmail.com>
2016-09-05 17:03:12 -0400

    race info

--- app/scripts/controllers/raceresults.controller.js
+++ app/scripts/controllers/raceresults.controller.js
@@ -2,16 +2,133 @@
 
 /**
  * @ngdoc function
- * @name brendanofferApp.controller:MainCtrl
+ * @name village.controller:MainCtrl
  * @description
  * # MainCtrl
- * Controller of the brendanofferApp
+ * Controller of the Village
  */
 angular.module('brendanofferApp')
   .controller('RaceResultsCtrl', function () {
     var vm = this;
 
     vm.races = [
+      {
+        label: "Green Mountain Stage 4",
+        type: 'cycling',
+        imageUrl: "/images/greenmountain-stage-4.jpg",
+        description: "The last stage of the Green Mountain Stage Race.",
+        races: [{
+          category:'3',
+          riders: [{name: "Rory O'neill", place: "43" resultsLink: "https://www.road-results.com/race/8231"}]
+        }],
+        resultsUrl: "https://www.road-results.com/race/8231",
+        date: "9/5/16"
+      },
+      {
+        label: "Green Mountain Stage 3",
+        type: 'cycling',
+        imageUrl: "/images/greenmountain-stage-3.jpg",
+        description: "The queen stage of the Green Mountain Stage Race.",
+        races: [{
+          category:'3',
+          riders: [{name: "Rory O'neill", place: "2" resultsLink: "https://www.road-results.com/race/8229"}]
+        }],
+        resultsUrl: "https://www.road-results.com/race/8229",
+        date: "9/4/16"
+      },
+      {
+        label: "Green Mountain Stage 2",
+        type: 'cycling',
+        imageUrl: "/images/greenmountain-stage-2.jpg",
+        description: "The second stage of the Green Mountain Stage Race.",
+        races: [{
+          category:'3',
+          riders: [{name: "Rory O'neill", place: "15" resultsLink: "https://www.road-results.com/race/8228"}]
+        }],
+        resultsUrl: "https://www.road-results.com/race/8228",
+        date: "9/3/16"
+      },
+      {
+        label: "Green Mountain Stage 1",
+        type: 'cycling',
+        imageUrl: "/images/greenmountain-stage-1.jpg",
+        description: "The first stage and timetrial of the Green Mountain Stage Race.",
+        races: [{
+          category:'3',
+          riders: [{name: "Rory O'neill", place: "10" resultsLink: "https://www.road-results.com/race/8227"}]
+        }],
+        resultsUrl: "https://www.road-results.com/race/8227",
+        date: "9/2/16"
+      },
+      {
+        label: "Lucarelli & Castaldi Race Series Round 6",
+        type: 'cycling',
+        imageUrl: "/images/16/lucarelli-round-6.jpg",
+        description: "The last race in the LC race series. ",
+        races: [{
+          category:'3',
+          riders: [
+            {name: "Rory O'neill", place: "37" resultsLink: "https://www.usacycling.org/results/index.php?permit=2016-1273"},
+          ]
+        },
+        {
+          category:'4',
+          riders: [
+            {name: "Nick Baker", place: "27" resultsLink: "https://www.usacycling.org/results/index.php?permit=2016-1273"},
+            {name: "Chris Cheetham", place: "14" resultsLink: "https://www.usacycling.org/results/index.php?permit=2016-1273"},
+            {name: "Brendan Offer", place: "2" resultsLink: "https://www.usacycling.org/results/index.php?permit=2016-1273"},
+          ]
+        }
+        ],
+        resultsUrl: "https://www.road-results.com/race/8143",
+        date: "8/14/16"
+      },
+      {
+        label: "Aetna Tokeneke Classic Road Race",
+        type: 'cycling',
+        imageUrl: "/images/16/tokeneke.jpg",
+        description: "Aetna Tokeneke Classic Road Race",
+        races: [{
+          category:'3',
+          riders: [
+            {name: "Rory O'neill", place: "9" resultsLink: "https://www.road-results.com/race/8143"},
+          ]
+        },
+        {
+          category:'4',
+          riders: [
+            {name: "Nick Baker", place: "7" resultsLink: "https://www.road-results.com/race/8143#r121654"}
+          ]
+        }
+        ],
+        resultsUrl: "https://www.road-results.com/race/8143",
+        date: "8/14/16"
+      },
+      {
+        label: "The Jersey Devil",
+        type: 'cycling',
+        imageUrl: "/images/16/jersey-devil.jpg",
+        description: "",
+        races: [{
+          category:'3',
+          riders: [{name: "Rory O'neill", place: "7" resultsLink: "https://www.road-results.com/race/8058#r113146"}]
+        }],
+        resultsUrl: "https://www.road-results.com/race/8058#r113146",
+        date: "8/7/16"
+      },
+      {
+        label: "Tour of the Hilltowns",
+        type: 'cycling',
+        imageUrl: "/images/16/hilltowns.jpg",
+        description: "",
+        races: [{
+          category:'3',
+          riders: [{name: "Rory O'neill", place: "6" resultsLink: "https://www.road-results.com/race/7916#r113146"}]
+        }],
+        resultsUrl: "https://www.road-results.com/race/7916#r113146",
+        date: "7/23/16"
+      },
+      
       {
         label: "Pass It Along Triathlon",
         imageUrl: "/images/pass-it-along-16.jpg",
